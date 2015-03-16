10a552642d212fc71d138245fa71d527d442bb01
Amin Sojoudi <masojoudi@gmail.com>
2015-03-16 12:26:42 +0330

    * Initial Commit

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationController : MonoBehaviour 
+{
+	private Animator animator;
+
+
+
+	void Start () 
+	{
+		animator = GetComponent<Animator>();
+	}
+	
+
+	void Update () 
+	{
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		animator.SetFloat("DirectionUPDown",v);
+		animator.SetFloat("DirectionLeftRight",h);
+	}
+}
